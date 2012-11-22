5b9a7e28adb70b1b1f69f296242f755758786c19
Alexis 'Horgix' Chotard <alexis.horgix.chotard@gmail.com>
2012-11-22 12:48:26 +0100

    [ALL] Install.sh now working properly

---
+++ install.sh
@@ -0,0 +1,52 @@
+#! /bin/sh
+
+Color_reset='\e[0;0m'
+
+Black='\e[0;30m'        # Black
+Red='\e[0;31m'          # Red
+Green='\e[0;32m'        # Green
+Yellow='\e[0;33m'       # Yellow
+Blue='\e[0;34m'         # Blue
+Purple='\e[0;35m'       # Purple
+Cyan='\e[0;36m'         # Cyan
+White='\e[0;37m'        # White
+
+BBlack='\e[1;30m'       # Bold Black
+BRed='\e[1;31m'         # Bold Red
+BGreen='\e[1;32m'       # Bold Green
+BYellow='\e[1;33m'      # Bold Yellow
+BBlue='\e[1;34m'        # Bold Blue
+BPurple='\e[1;35m'      # Bold Purple
+BCyan='\e[1;36m'        # Bold Cyan
+BWhite='\e[1;37m'       # Bold White
+
+to_install=`ls -A . | grep -Ev 'TODO|README|install.sh|.git'`
+
+for file in $to_install; do
+    echo -e "${Yellow}\nInstalling $file...${Color_reset}"
+    target=~/$file
+    src=`pwd`/$file
+    if [ -e $target ]; then
+        echo -n "$target already exists. Replace it ? (Y/n) : "
+        read replace
+        if [ -z $replace ]; then
+            replace="Y"
+        fi
+        echo $replace
+        if [ $replace = "Y" ] || [ $replace = "y" ]; then
+            echo "Removing $target..."
+            rm -R ~/$file
+            echo -e "${BRed}$target removed.${Color_reset}"
+            echo "Creating Symlink $target on $src..."
+            ln -is `pwd`/$file ~/
+            echo -e "${BGreen}Symlink created."
+        else
+            echo -e "${BRed}$file skipped."
+        fi
+    else
+        echo "Creating Symlink $target on $src..."
+        ln -is `pwd`/$file ~/
+        echo -e "${BGreen}Symlink created."
+    fi
+    echo -en ${Color_reset}
+done
