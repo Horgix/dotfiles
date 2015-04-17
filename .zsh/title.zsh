6686f52a68f74660a7b2d56c151421c6e900b815
Alexis 'Horgix' Chotard <alexis.horgix.chotard@gmail.com>
2015-04-17 18:14:48 +0200

    [ZSH] title: fix on rxvt variants

--- .zsh/title.zsh
+++ .zsh/title.zsh
@@ -8,12 +8,12 @@
 function set_title {
     a=${(V)1//\%/\%\%}
     a=$(print -Pn "%40>...>$a" | tr -d "\n")
-  
+
     case $TERM in
     screen*)
       print -Pn "\ek$a:$3\e\\"      # screen title (in ^A")
       ;;
-    xterm*|rxvt)
+    xterm*|*rxvt*)
       print -Pn "\e]2;$2 | $a:$3\a" # plain xterm title
       ;;
     esac
